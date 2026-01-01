6ea48af99085ae52bd9bcdbdbfa0eaf7ee2a2e68
prodiinformatika82-oss <prodiinformatika82@gmail.com>
2026-01-01 15:02:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+import streamlit as st
+
