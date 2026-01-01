c057e1dfa431f028afd3a6a94c0f3f4317a8387b
prodiinformatika82-oss <prodiinformatika82@gmail.com>
2026-01-01 15:08:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("KASIR AICE MARKET")
-st.title("_dapatkan kenyaman_  :blue[Berbelanja] :sunglasses:")
+st.title("_dapatkan kenyaman_ is :blue[Berbelanja] :sunglasses:")
 
 
